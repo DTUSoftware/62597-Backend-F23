ca9178f78f8eab938c3308aaab7c2c212745ef03
Rasm-P <rasmus.pra@gmail.com>
2023-02-17 18:33:16 +0100

    Ínitial work on EF core implementation

---
+++ ShopBackend/Contexts/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopBackend.Models;
+
+namespace ShopBackend.Contexts
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<Product> OnlineAccounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { }
+    }
+}
---
+++ ShopBackend/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ShopBackend.Models
+
+{
+    public record Product
+    {
+        [Key]
+        public String Id { get; init; }
+
+        [Required(ErrorMessage = "Name cannot be empty")]
+        [StringLength(100)]
+        public String Name { get; init; }
+
+        [Required(ErrorMessage = "Price cannot be empty")]
+        public int Price { get; init; }
+
+        [Required(ErrorMessage = "Currency cannot be empty")]
+        [StringLength(100)]
+        public String Currency { get; init; }
+
+        [Required(ErrorMessage = "RebateQuantity cannot be empty")]
+        public int RebateQuantity { get; init; }
+
+        [Required(ErrorMessage = "RebatePercent cannot be empty")]
+        public int RebatePercent { get; init; }
+
+        public String? UpsellProductId { get; init; }
+    }
+}
